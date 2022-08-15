c587a5a34dd0ac9b83678b64095fc633b945a49a
scm-wutyitheint <scm.dott.developer10@gmail.com>
2022-08-15 16:53:45 +0630

    add python setting and login page

---
+++ nuxt_side/layouts/default.vue
@@ -0,0 +1,83 @@
+<template>
+    <div>
+        <nav>
+            <ul>
+                <li>
+                    <NuxtLink to="/">Bulletin Board</NuxtLink>
+                </li>
+
+                <li>
+                    <NuxtLink to="/">Home</NuxtLink>
+                </li>
+                <li>
+                    <NuxtLink to="/">Parent</NuxtLink>
+                </li>
+            </ul>
+        </nav>
+        <main>
+            <Nuxt />
+        </main>
+    </div>
+</template>
+
+<style>
+:root {
+    --primary-color: #00c58e;
+}
+
+body {
+    font-family: system-ui, -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
+        Helvetica Neue, Arial, Noto Sans, sans-serif, Apple Color Emoji,
+        Segoe UI Emoji, Segoe UI Symbol, Noto Color Emoji;
+    margin: 0;
+    padding: 0;
+}
+
+a {
+    color: inherit;
+    text-decoration: none;
+}
+
+nav a:hover {
+    text-decoration: none;
+}
+
+a:hover {
+    text-decoration: underline;
+}
+
+main {
+    margin: 0 auto;
+    margin-top: 100px;
+    padding: 0 1rem;
+    max-width: 1280px;
+    text-align: center;
+}
+
+img {
+    margin-bottom: 1rem;
+}
+
+ul {
+    list-style-type: none;
+    padding: 0 60px;
+    display: flex;
+    justify-content: left;
+    flex-wrap: wrap;
+    background-color: antiquewhite;
+}
+
+li {
+    margin: 0 0.5rem;
+    padding: 0.6rem;
+    font-size: 1.2rem;
+}
+
+nav {
+    /* padding: 0 1rem; */
+}
+
+a.nuxt-link-exact-active {
+    color: #00c58e;
+}
+</style>
--- nuxt_side/pages/index.vue
+++ nuxt_side/pages/index.vue
@@ -1,5 +1,26 @@
 <template>
-  <Tutorial/>
+  <div class="container">
+    <h1 class="title">
+      Login Form
+    </h1>
+    <div class="content">
+      <form name="contact" action="" method="post">
+        <div class="section">
+          <label class="form-label" for="name">
+            Email:
+          </label>
+          <input class="form-field" type="email" name="name" id="name" />
+        </div>
+        <div class="section">
+          <label class="form-label" for="email">
+            Password:
+          </label>
+          <input class="form-field" type="password" name="email" id="email" />
+        </div>
+        <button class="form-button" type="submit">Submit</button>
+      </form>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -7,3 +28,34 @@ export default {
   name: 'IndexPage'
 }
 </script>
+
+<style>
+.container {
+  margin: 0 auto;
+  /* margin-top: 100px; */
+  padding: 0 1rem;
+  max-width: 1280px;
+  text-align: center;
+  font-size: 1.3em;
+}
+
+label {
+  padding: 50px;
+}
+
+input {
+  padding: 8px;
+  width: 400px;
+}
+
+.section {
+  display: block;
+  padding: 10px;
+}
+
+button {
+  margin-top: 20px;
+  padding: 8px 20px 8px 20px;
+  font-size: 0.8em;
+}
+</style>
